cddd157fcafff93d666c9534abbd5dd35ee03939
Fernando Lucius <fernando.muramasa@gmail.com>
2021-07-27 09:55:57 -0300

    First commit

---
+++ pocIdentity/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pocIdentity.Models
+{
+    public class Produto
+    {
+        public int ID { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataInclusao { get; set; }
+    }
+}
